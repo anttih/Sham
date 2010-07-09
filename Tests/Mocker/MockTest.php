a3184d30defd58a426c8c98a8d2ecc2b62c31bff
Antti Holvikari <anttih@gmail.com>
2010-07-09 11:10:09 +0300

    Set mock object in test setup

--- Tests/Mocker/MockTest.php
+++ Tests/Mocker/MockTest.php
@@ -3,138 +3,128 @@ require_once 'Mocker/Mock.php';
 
 class Mocker_MockTest extends PHPUnit_Framework_TestCase
 {
+    public function setup()
+    {
+        $this->mock = new Mocker_Mock();
+    }
+
     public function testCallingMethodsShouldReturnMocker_Mock()
     {
-        $mocker = new Mocker_Mock();
-        $ret = $mocker->method();
+        $ret = $this->mock->method();
         $this->assertTrue($ret instanceof Mocker_Mock);
     }
     
     public function testGettingAPropertyWithoutSettingShouldReturnCallObject()
     {
-        $mocker = new Mocker_Mock();
-        $this->assertTrue($mocker->property instanceof Mocker_Call);
+        $this->assertTrue($this->mock->property instanceof Mocker_Call);
     }
 
     public function testGettingAPropertyWithoutSettingShouldNotRecord()
     {
-        $mocker = new Mocker_Mock();
-        $value = $mocker->key;
-        $this->assertFalse($mocker->calls('__get', 'key')->once());
+        $value = $this->mock->key;
+        $this->assertFalse($this->mock->calls('__get', 'key')->once());
     }
 
     public function testShouldRecordPropertyGetWhenSet()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->key = 'value';
-        $value = $mocker->key;
+        $this->mock->key = 'value';
+        $value = $this->mock->key;
 
-        $this->assertTrue($mocker->calls('__get', 'key')->once());
+        $this->assertTrue($this->mock->calls('__get', 'key')->once());
 
-        $call = $mocker->calls('__get', 'key')->calls[0];
+        $call = $this->mock->calls('__get', 'key')->calls[0];
         $this->assertEquals('value', $call->return_value);
     }
 
     public function testShouldReturnSetPropertyValue()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->key = "key value";
-        $this->assertEquals("key value", $mocker->key);
+        $this->mock->key = "key value";
+        $this->assertEquals("key value", $this->mock->key);
     }
 
     public function testShouldRecordPropertySet()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->key = 'value';
-        $this->assertTrue($mocker->calls('__set', 'value')->once());
+        $this->mock->key = 'value';
+        $this->assertTrue($this->mock->calls('__set', 'value')->once());
     }
 
     public function testShouldReturnFromChildMocker_Mock()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->method->setReturn("return value");
-        $this->assertEquals("return value", $mocker->method());
+        $this->mock->method->setReturn("return value");
+        $this->assertEquals("return value", $this->mock->method());
     }
 
     public function testShouldAllowFalsyReturnValues()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->someMethod->setReturn(false);
-        $this->assertEquals(false, $mocker->someMethod());
+        $this->mock->someMethod->setReturn(false);
+        $this->assertEquals(false, $this->mock->someMethod());
     }
 
     public function testShouldRecordInvoke()
     {
-        $mocker = new Mocker_Mock();
-        $mocker();
-        $this->assertTrue($mocker->calls('__invoke')->once());
+        $mock = new Mocker_Mock();
+        $mock();
+        $this->assertTrue($mock->calls('__invoke')->once());
     }
 
-    public function testShouldReturnThisMockIfNoReturnValueSet()
+    public function testInvokeShouldReturnThisMockIfNoReturnValueSet()
     {
-        $mocker = new Mocker_Mock();
-        $this->assertSame($mocker, $mocker());
+        $mock = new Mocker_Mock();
+        $this->assertSame($mock, $mock());
     }
 
     public function testShouldUseReturnValueWhenInvoked()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->setReturn('return value');
-        $this->assertSame('return value', $mocker());
+        $mock = new Mocker_Mock();
+        $mock->setReturn('return value');
+        $this->assertSame('return value', $mock());
     }
 
     public function testShouldRecordCalls()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->method0();
-        $mocker->method1();
-        $this->assertEquals(2, count($mocker->calls()));
+        $this->mock->method0();
+        $this->mock->method1();
+        $this->assertEquals(2, count($this->mock->calls()));
     }
 
     public function testShouldRecordReturnValue()
     {
-        $mocker = new Mocker_Mock();
-        $mocker->method();
-        $this->assertTrue($mocker->calls()->calls[0]->return_value instanceof Mocker_Mock);
+        $this->mock->method();
+        $this->assertTrue($this->mock->calls()->calls[0]->return_value instanceof Mocker_Mock);
     }
 
     public function testShouldProxyCallsToCallList()
     {
-        $mocker = new Mocker_Mock();
-        $this->assertTrue($mocker->calls() instanceof Mocker_CallList);
+        $this->assertTrue($this->mock->calls() instanceof Mocker_CallList);
     }
 
     public function testOffsetSetShouldSetValue()
     {
-        $mock = new Mocker_Mock();
-        $mock[0] = 1;
-        $data = $mock->mockerGetData();
+        $this->mock[0] = 1;
+        $data = $this->mock->mockerGetData();
         $this->assertEquals(1, $data[0]);
     }
 
     public function testShouldRecordOffsetSet()
     {
-        $mock = new Mocker_Mock();
-        $mock[0] = 1;
-        $this->assertTrue($mock->calls('offsetSet', 0, 1)->once());
+        $this->mock[0] = 1;
+        $this->assertTrue($this->mock->calls('offsetSet', 0, 1)->once());
     }
 
     public function testOffsetGetShouldGetValue()
     {
-        $mock = new Mocker_Mock();
-        $mock->mockerSetData(array(1));
-        $this->assertEquals(1, $mock[0]);
+        $this->mock->mockerSetData(array(1));
+        $this->assertEquals(1, $this->mock[0]);
     }
 
     public function testShouldRecordOffsetGet()
     {
-        $mock = new Mocker_Mock();
-        $mock->mockerSetData(array(1));
+        $this->mock->mockerSetData(array(1));
 
         // action
-        $mock[0];
+        $this->mock[0];
 
-        $list = $mock->calls('offsetGet', 0);
+        $list = $this->mock->calls('offsetGet', 0);
         $this->assertTrue($list->once());
 
         // records return value
@@ -143,27 +133,24 @@ class Mocker_MockTest extends PHPUnit_Framework_TestCase
 
     public function testShouldRecordIsset()
     {
-        $mock = new Mocker_Mock();
-        $mock->mockerSetData(array(1, 2));
-        isset($mock[0]);
-        $this->assertTrue($mock->calls('offsetExists', 0)->once());
+        $this->mock->mockerSetData(array(1, 2));
+        isset($this->mock[0]);
+        $this->assertTrue($this->mock->calls('offsetExists', 0)->once());
     }
 
     public function testShouldIssetReturnValueAsCallReturnValue()
     {
-        $mock = new Mocker_Mock();
-        $mock->mockerSetData(array(1, 2));
-        $isset = isset($mock[0]);
+        $this->mock->mockerSetData(array(1, 2));
+        $isset = isset($this->mock[0]);
         $this->assertTrue($isset);
-        $this->assertTrue($mock->calls('offsetExists', 0)->calls[0]->return_value);
+        $this->assertTrue($this->mock->calls('offsetExists', 0)->calls[0]->return_value);
     }
 
     public function testShouldRecordUnset()
     {
-        $mock = new Mocker_Mock();
-        $mock->mockerSetData(array(1, 2));
-        unset($mock[0]);
-        $this->assertTrue($mock->calls('offsetUnset', 0)->once());
+        $this->mock->mockerSetData(array(1, 2));
+        unset($this->mock[0]);
+        $this->assertTrue($this->mock->calls('offsetUnset', 0)->once());
     }
 }
 
