56c5c31d4c27f3547c4663b5471787633b3f5910
Antti Holvikari <anttih@gmail.com>
2010-11-05 20:05:43 +0200

    Copy&paste error

--- Sham.php
+++ Sham.php
@@ -28,7 +28,7 @@ use Sham\Builder,
  *   disclaimer in the documentation and/or other materials provided
  *   with the distribution.
  * 
- * * Neither the name of the Solar project nor the names of its
+ * * Neither the name of the Sham project nor the names of its
  *   contributors may be used to endorse or promote products derived
  *   from this software without specific prior written permission.
  * 
